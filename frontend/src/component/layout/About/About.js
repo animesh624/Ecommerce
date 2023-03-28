7ed793779997abb5bb1a36207057ff52b1696df4
animesh624 <animesh624d@gmail.com>
2023-03-28 15:15:48 +0530

    added contact and about me

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -1,46 +1,47 @@
 import React from "react";
 import "./aboutSection.css";
 import { Button, Typography, Avatar } from "@material-ui/core";
-import YouTubeIcon from "@material-ui/icons/YouTube";
-import InstagramIcon from "@material-ui/icons/Instagram";
+// import YouTubeIcon from "@material-ui/icons/YouTube";
+import CodeIcon from '@mui/icons-material/Code';
+// import InstagramIcon from "@material-ui/icons/Instagram";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 const About = () => {
-  const visitInstagram = () => {
-    window.location = "https://instagram.com/meabhisingh";
+  const visitLinkedIn = () => {
+    window.location = "https://www.linkedin.com/in/animesh624d/";
   };
   return (
     <div className="aboutSection">
       <div></div>
       <div className="aboutSectionGradient"></div>
       <div className="aboutSectionContainer">
-        <Typography component="h1">About Us</Typography>
+        <Typography component="h1">About Me</Typography>
 
         <div>
           <div>
             <Avatar
               style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
+              src="https://media.licdn.com/dms/image/C4D03AQEFHk1hZpkniw/profile-displayphoto-shrink_400_400/0/1662570626961?e=1685577600&v=beta&t=sSg-jwJU0-_KqynSns1AcSTYF5tQHuMyz0T0auleTeo"
               alt="Founder"
             />
-            <Typography>Abhishek Singh</Typography>
-            <Button onClick={visitInstagram} color="primary">
-              Visit Instagram
+            <Typography>Animesh Dubey</Typography>
+            <Button onClick={visitLinkedIn} color="primary">
+              Visit LinkedIn
             </Button>
             <span>
-              This is a sample wesbite made by @meabhisingh. Only with the
-              purpose to teach MERN Stack on the channel 6 Pack Programmer
+              This is a sample wesbite made by @animesh624d.
             </span>
           </div>
           <div className="aboutSectionContainer2">
-            <Typography component="h2">Our Brands</Typography>
+            <Typography component="h2">Social Handles</Typography>
             <a
-              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+              href="https://codeforces.com/profile/animesh624d"
               target="blank"
             >
-              <YouTubeIcon className="youtubeSvgIcon" />
+              <CodeIcon className="codeforcesSvgIcon" />
             </a>
 
-            <a href="https://instagram.com/meabhisingh" target="blank">
-              <InstagramIcon className="instagramSvgIcon" />
+            <a href="https://www.linkedin.com/in/animesh624d/" target="blank">
+              <LinkedInIcon className="linkedinSvgIcon" />
             </a>
           </div>
         </div>
--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -14,17 +14,17 @@ const Footer = () => {
       </div>
 
       <div className="midFooter">
-        <h1>ECOMMERCE.</h1>
+        <h1>ECOMMERCE</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; MeAbhiSingh</p>
+        <p>Copyrights 2021 &copy; animesh624d</p>
       </div>
 
       <div className="rightFooter">
-        <h4>Follow Us</h4>
-        <a href="http://instagram.com/meabhisingh">Instagram</a>
-        <a href="http://youtube.com/6packprogramemr">Youtube</a>
-        <a href="http://instagram.com/meabhisingh">Facebook</a>
+        <h4>You can Follow Me</h4>
+        <a href="https://www.linkedin.com/in/animesh624d/">LinkedIn</a>
+        <a href="https://codeforces.com/profile/animesh624d">Codeforces</a>
+        <a href="https://github.com/animesh624">Github</a>
       </div>
     </footer>
   );
